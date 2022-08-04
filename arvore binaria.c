79f75dbcd8c2e08ec20f0209450c6e1dabec71ce
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-08-04 18:27:35 -0300

    Add files via upload

---
+++ arvore binaria.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct arv
+{
+    int info;
+    struct arv *esq;
+    struct arv *dir;
+} arvore;
+arvore *raiz;
+
+arvore* insere(arvore *root, arvore *r, int dado)
+{
+    if(r==NULL)
+    {
+        r = (arvore *) malloc(sizeof(arvore));
+        if(r==NULL)
+            printf("\nMemoria Insufuciente !!!");
+        else
+        {
+            r->esq=NULL;
+            r->dir=NULL;
+            r->info=dado;
+            if(root==NULL)
+                return r;
+            if(r->info <= root->info)
+                root->esq = r;
+            else
+                root->dir = r;
+            return r;
+        }
+    }
+    if(dado < r->info)
+        insere(r,r->esq,dado);
+    else
+        insere(r,r->dir,dado);
+    return r;
+}
+
+void PreOrdem(arvore *root)
+{
+    if(root==NULL)
+        return;
+    printf("\n%d",root->info);
+    PreOrdem(root->esq);
+    PreOrdem(root->dir);
+}
+
+void EmOrdem(arvore *root)
+{
+    if(root==NULL)
+        return;
+    EmOrdem(root->esq);
+    printf("\n%d",root->info);
+    EmOrdem(root->dir);
+}
+
+void PosOrdem(arvore *root)
+{
+    if(root==NULL)
+        return;
+    PosOrdem(root->esq);
+    PosOrdem(root->dir);
+    printf("\n%d",root->info);
+}
+
+void main()
+{
+    int valor, opcao;
+    raiz=NULL;
+    while(opcao != 0)
+    {
+        printf("\nMenu \n1 Inserir \n2 Pre-Ordem \n3 Em-Ordem \n4 Pos-Ordem \n0 Sair\n");
+        scanf("%d",&opcao);
+        if(opcao == 1)
+        {
+            printf("\nDigite o valor que quer colocar: ");
+            scanf("%d",&valor);
+
+            if(raiz==NULL)
+                raiz=insere(raiz,raiz,valor);
+            else
+                insere(raiz,raiz,valor);
+
+        }
+        else if(opcao ==2)
+        {
+            printf("\n\nPre-Ordem : ");
+            PreOrdem(raiz);
+        }
+        else if(opcao ==3)
+        {
+            printf("\n\nEm-Ordem : ");
+            EmOrdem(raiz);
+        }
+        else if(opcao ==4)
+        {
+            printf("\n\nPos-Ordem : ");
+            PosOrdem(raiz);
+        }
+    }
+}
